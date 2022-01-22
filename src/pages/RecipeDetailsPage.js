7a3a290d67d6c85efd27ecaa4f493ef80bb774ad
Monica Saiz <moni.sm@gmail.com>
2022-01-22 12:29:10 +0100

    deleting used code

--- src/pages/RecipeDetailsPage.js
+++ src/pages/RecipeDetailsPage.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import AddTask from "../components/AddTask";
 import RecipeCard from "../components/recipes/RecipeCard";
 
 const API_URI = process.env.REACT_APP_API_URL;
@@ -37,8 +36,6 @@ function RecipeDetailsPage(props) {
           <p>{recipe.description}</p>
         </>
       )}
-
-      <AddTask refreshRecipe={getRecipe} recipeId={recipeId} />
 
       {recipe &&
         recipe.tasks.map((task) => <RecipeCard key={task._id} {...task} />)}
